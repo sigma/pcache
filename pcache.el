fa8f863546e2e8f2fc0a70f5cc766a7f584e01b6
Yann Hodique <yann.hodique@gmail.com>
2012-04-08 12:06:41 +0200

    bump version to 0.2.3

--- pcache.el
+++ pcache.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yann Hodique <yann.hodique@gmail.com>
 ;; Keywords:
-;; Version: 0.2.2
+;; Version: 0.2.3
 ;; Package-Requires: ((eieio "1.3"))
 
 ;; This file is free software; you can redistribute it and/or modify
