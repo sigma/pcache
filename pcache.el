099795e5796219f73f7a18042d2fa30bb40cfddf
Yann Hodique <hodiquey@vmware.com>
2015-01-25 16:38:12 -0800

    bump version to 0.3.0

--- pcache.el
+++ pcache.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yann Hodique <yann.hodique@gmail.com>
 ;; Keywords:
-;; Version: 0.2.4
+;; Version: 0.3.0
 ;; Package-Requires: ((eieio "1.3"))
 
 ;; This file is free software; you can redistribute it and/or modify
